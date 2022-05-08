4bb5cc571e8682b8b00103392339f5361202ce88
mattheo <mattheocognet@gmail.com>
2022-05-08 15:09:34 +0200

    ajout de themes

--- Crucial.py
+++ Crucial.py
@@ -10,6 +10,10 @@ lst_joueur = []
 
 def choix_joueur():
     print("Tapez 1 pour sciences")
+    print("Tapez 2 pour Mathématiques")
+    print("Tapez 3 pour Histoires")
+    print("Tapez 4 pour Géographie")
+    print("Tapez 5 pour Francais")
     choix = int(input())
     question_choix(choix)
 
@@ -23,17 +27,86 @@ def question_choix(choix):
         print("Question : ", quest[alea])  # ecrit une question de facon aléatoires 
         reponse = str(input())
         reponse_sciences(reponse, alea)
+    elif choix == 2:
+        quest = question_maths() #apprends la liste des questions
+        alea = random.randint(0, 1)
+        print("Question : ", quest[alea])  # ecrit une question de facon aléatoires 
+        reponse = str(input())
+        reponse_maths(reponse, alea)
+    elif choix == 3:
+        quest = question_hist() #apprends la liste des questions
+        alea = random.randint(0, 1)
+        print("Question : ", quest[alea])  # ecrit une question de facon aléatoires 
+        reponse = str(input())
+        reponse_hist(reponse, alea)
+    elif choix == 4:
+        quest = question_geo() #apprends la liste des questions
+        alea = random.randint(0, 1)
+        print("Question : ", quest[alea])  # ecrit une question de facon aléatoires 
+        reponse = str(input())
+        reponse_geo(reponse, alea)
+    elif choix == 5:
+        quest = question_francais() #apprends la liste des questions
+        alea = random.randint(0, 1)
+        print("Question : ", quest[alea])  # ecrit une question de facon aléatoires 
+        reponse = str(input())
+        reponse_francais(reponse, alea)
 
 def question_sciences():
     '''stock les questions sciences'''
     quest = ["Quel est le symbole atomique du soufre ?", "Quelle particule est neutre ?"]
     return(quest)
 
+def question_maths():
+    '''stock les questions math'''
+    quest = ["Quel théorème permet d'avoir l'hypothénuse ?", "Apres milliard il y'a ..."]
+    return(quest)
+
+def question_hist():
+    '''stock les questions histoires'''
+    quest = ["Qui c'est fait décapiter le 16 Mai 68", "Quand se fini la seconde guerre mondiale ?"]
+    return(quest)
+
+def question_geo():
+    '''stock les questions geo'''
+    quest = ["Quelle est la capital de la Suisse ?", "A qui appartient le groenland ?"]
+    return(quest)
+
+def question_francais():
+    '''stock les questions francais'''
+    quest = ["Pluriel du mot vitrail", "La pizza de vito est pour ses/ces parents."]
+    return(quest)
+
 def reponse_sciences(reponse, alea):
     '''stock les réponses sciences'''
+    reponses = ["s","neutron"]
+    score_game(reponses, reponse, alea)
+
+def reponse_maths(reponse, alea):
+    '''stock les réponses maths'''
+    reponses = ["Pythagore","billion"]
+    score_game(reponses, reponse, alea)
+
+def reponse_hist(reponse, alea):
+    '''stock les réponses histoire'''
+    reponses = ["Louis 16","1945"]
+    score_game(reponses, reponse, alea)
+    
+def reponse_geo(reponse, alea):
+    '''stock les réponses geo'''
+    reponses = ["Berne","Danemark"]
+    score_game(reponses, reponse, alea)
+
+def reponse_francais(reponse, alea):
+    '''stock les réponses geo'''
+    reponses = ["vitraux","ses"]
+    score_game(reponses, reponse, alea)
+    
+    
+def score_game(reponses, reponse, alea):
+    '''calcule le score'''
     global score
-    reponses_sciences = ["S","neutron"]
-    if reponses_sciences[alea] == reponse:
+    if reponses[alea] == reponse:
         score[J] = score[J]+1
         print("Score : ", score[J])
         return score
@@ -43,8 +116,14 @@ def reponse_sciences(reponse, alea):
         return score
     
 def joueur():
+    global score
     global nb_joueur
     global lst_joueur
+    print("Bienvenue sur Crucial Test !!")
+    print("Voulez vous lire les règles ?")
+    reglement = str(input())
+    if reglement == "oui":
+        regle()
     print("Combien de joueur êtes vous ?")
     nb_joueur = int(input())
     print("Donnez le nom des joueurs")
@@ -92,5 +171,16 @@ def jeu():
                     else:
                         print("au revoir")
 
+def regle():
+    print("Voici les règles du Crucial Test: ")
+    print("Vous devrez choisir entre plusieurs thèmes,")
+    print("le but est de marquer le plus de points pour remporter.")
+    print("Seul les noms propres preinnent une majuscules au début.")
+    print("Une réponse court et direct est attendu, exemple :")
+    print("Capital de la Suisse?")
+    print("Berne")
+    print("Donc ne pas faire de longue phrases sinon votre réponse sera fausses.")
+    print("Bon jeu")
 
 jeu()
+
